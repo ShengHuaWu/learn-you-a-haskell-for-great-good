bb6585dd8437c9fa6f2a223e618469b17de498dd
ShengHua Wu <swu1@wayfair.com>
2020-05-27 12:05:06 +0200

    Append homework 2 content.

---
+++ homeworks/hw2.hs
@@ -0,0 +1,42 @@
+-- https://www.seas.upenn.edu/~cis194/spring13/hw/02-ADTs.pdf
+
+import Data.Char
+
+data MessageType = Info
+                    | Warning
+                    | Error Int
+                    deriving (Show, Eq)
+
+type TimeStamp = Int
+
+data LogMessage = LogMessage MessageType TimeStamp String 
+                    | Unknown String
+                    deriving (Show, Eq)
+
+readFirstInt :: String -> (Int, String)
+readFirstInt text = (read . head $ xs, unwords . tail $ xs)
+    where
+        xs = words text
+
+parseMessageType :: String -> (Maybe MessageType, String)
+parseMessageType (x:xs)
+    | x == 'I' = (Just Info, tail xs)
+    | x == 'W' = (Just Warning, tail xs)
+    | x == 'E' = (Just (Error level), reminder)
+    | otherwise = (Nothing, x:xs)
+    where
+        (level, reminder) = readFirstInt xs
+
+parseMessage :: String -> LogMessage
+parseMessage text = case first of
+    Just messageType -> LogMessage messageType time message
+    Nothing          -> Unknown reminder
+    where
+        (first, reminder) = parseMessageType text
+        (time, message) = readFirstInt reminder
+
+main = do
+    print (parseMessage "W 1234 This is a warning")
+    print (parseMessage "I 6543 This is a information")
+    print (parseMessage "E 99 124 This is an error")
+    print (parseMessage "K xyz whatever")
